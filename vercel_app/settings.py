6fef10b907480144ebf9893c3a3ebf5c68276583
Dhanashree Mhatre <93194086+dhanashreemhatre@users.noreply.github.com>
2024-04-02 19:56:37 +0530

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -146,9 +146,9 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static'] 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR / 'static'))
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Media Root - The directory where uploaded files will be stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
