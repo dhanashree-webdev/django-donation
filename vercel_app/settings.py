cc614faedfc5c29b97b9ad8099031cd6517c9b34
Dhanashree Mhatre <93194086+dhanashreemhatre@users.noreply.github.com>
2024-04-14 21:22:27 +0530

    Fixed settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
     'https://inclusify-donation-platform.vercel.app'
 ]
 
